ec8f106e9722ed8f622294767b2583e3f141f044
Chris Simpson <c.a.simpson01@gmail.com>
2016-07-19 08:50:10 +0100

    Updated setup.py and MANIFEST.

--- setup.py
+++ setup.py
@@ -1,23 +1,20 @@
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup
 
 
 setup(
-    name='xrpd',
+    name='xrdpd',
     version='0.1.0',
     author='C. Simpson',
     author_email='c.a.simpson01@gmail.com',
     packages=['xrdpb'],
-    include_package_data = True,
+    include_package_data=True,
     url='https://github.com/casimp/xrdpb',
     download_url = 'https://github.com/casimp/xrdpb/tarball/v0.1.0',
     license='LICENSE.txt',
     description='Create 1D XRD patterns and 2D Debye-Scherrer ringe for simple material systems/diffraction setups.',
     keywords = ['XRD', 'EDXD', 'x-ray', 'diffraction', 'strain', 'synchrotron'],
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 4 - Beta",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.4",
